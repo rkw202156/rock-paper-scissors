bcb37368c0a23b7a2b3b9858f1346ec0a80cdfd6
rkw202156 <reynold.k.wong@gmail.com>
2023-06-16 23:38:03 -0700

    Finish script.js

--- script.js
+++ script.js
@@ -4,4 +4,37 @@ function getComputerChoice() {
     return CHOICES[choice]
 }
 
-console.log(getComputerChoice())
+function getUserChoice() {
+    let choice = ""
+    do {
+        choice = prompt("Rock, Paper, Scissors: ").toLowerCase()
+    } while (choice !== "rock" && choice !== "paper" && choice !== "scissors")
+
+    return choice
+}
+
+function game(playerSelection, computerSelection) {
+    let returnString = ""
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection == computerSelection) {
+        returnString = "Tie!"
+    } else {
+        if (playerSelection == "rock") {
+            returnString =  computerSelection == "scissors" ? "You Win! Rock beats Scissors" : "You Lose! Paper beats Rock"
+        } else if (playerSelection == "paper") {
+            returnString = computerSelection == "rock" ? "You win! Paper beats Rock" : "You Lose! Scissors beats Paper"
+        } else {
+            returnString = computerSelection == "paper" ? "You win! Scissors beats Paper" : "You Lose! Rock beats scissors"
+        }
+    }
+
+    return returnString
+}
+
+const main = () => {
+    for (let i = 0; i < 5; i++) {
+        console.log(game(getUserChoice(), getComputerChoice()))
+    }
+}
+
+main()
